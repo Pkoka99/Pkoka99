1a38571c670f10f198e32f6789f545eee0884649
Pkoka99 <zekazeus123@gmail.com>
2025-04-28 22:38:19 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -102,7 +102,7 @@ for i, (email, password) in enumerate(accounts[:50]):
     thread = threading.Thread(target=open_browser, args=(i, email, password))
     thread.start()
     threads.append(thread)
-    time.sleep(20)  # Prevent overload
+    time.sleep(40)  # Prevent overload
 
 for thread in threads:
     thread.join()
