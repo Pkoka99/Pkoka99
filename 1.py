1f6ecb25ffc31c97453c9197a929b7db619eea55
Pkoka99 <zekazeus123@gmail.com>
2025-04-30 14:39:01 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -97,7 +97,7 @@ def open_browser(instance_number, email, password):
 
 # Launch multiple instances
 threads = []
-for i, (email, password) in enumerate(accounts[:100]):
+for i, (email, password) in enumerate(accounts[:50]):
     
     thread = threading.Thread(target=open_browser, args=(i, email, password))
     thread.start()
