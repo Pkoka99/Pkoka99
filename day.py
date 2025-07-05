57472635c84d103afa852307c88f9f06a6799e97
Pkoka99 <zekazeus123@gmail.com>
2025-07-06 01:13:59 +0700

    Create day.py

---
+++ day.py
@@ -0,0 +1,18 @@
+from daytona import Daytona, DaytonaConfig
+  
+# Define the configuration
+config = DaytonaConfig(api_key="dtn_c6d9c69f5c0d408d00215d9d456ac7747dc63cd44f454270d437a4332d934f57")
+
+# Initialize the Daytona client
+daytona = Daytona(config)
+
+# Create the Sandbox instance
+sandbox = daytona.create()
+
+# Run the code securely inside the Sandbox
+response = sandbox.process.code_run('import os\nos.system("lscpu")')
+if response.exit_code != 0:
+  print(f"Error: {response.exit_code} {response.result}")
+else:
+    print(response.result)
+  
